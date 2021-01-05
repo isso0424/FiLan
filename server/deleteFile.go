eb60c3c7293f4b398471223f67dc1fb0b7b242f5
isso0424 <isso04240129@gmail.com>
2021-01-05 22:14:21 +0900

    feat: create DeleteFile

--- server/deleteFile.go
+++ server/deleteFile.go
@@ -3,6 +3,32 @@ package server
 import "net/http"
 
 func deleteFileHandler(w http.ResponseWriter, r *http.Request) {
-	// nolint:godox
-	// TODO: write here
+	const endpoint = "file"
+	const method = "DELETE"
+	var name string
+	var path string
+	err := decoder.Decode(name, r.URL.Query())
+	if err != nil {
+		errorMessage := "Need query parameter: name"
+		handlerRequestError(w, endpoint, method, http.StatusBadRequest, errorMessage)
+
+		return
+	}
+	err = decoder.Decode(path, r.URL.Query())
+	if err != nil {
+		errorMessage := "Need query parameter: path"
+		handlerRequestError(w, endpoint, method, http.StatusBadRequest, errorMessage)
+
+		return
+	}
+
+	file, err := controller.DeleteFile(name, path)
+	if err != nil {
+		errorMessage := err.Error()
+		handlerRequestError(w, endpoint, method, http.StatusBadRequest, errorMessage)
+
+		return
+	}
+
+	domainWritebackToClient(file, w, endpoint, method)
 }
