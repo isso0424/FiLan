06cae83ff8b94117f88f2fd1c1f7769dc8d5e8fb
Kota Isozaki <49902177+isso0424@users.noreply.github.com>
2021-01-05 22:55:28 +0900

    feat: create DELETE /file (#28)
    * feat: create DeleteFile
    
    * feat: define queryNotEnoughError
    
    * feat: replace queryNotEnoughError
    
    * fix: lint
    
    * fix: lint

--- server/deleteFile.go
+++ server/deleteFile.go
@@ -3,6 +3,31 @@ package server
 import "net/http"
 
 func deleteFileHandler(w http.ResponseWriter, r *http.Request) {
-	// nolint:godox
-	// TODO: write here
+	const endpoint = "file"
+	const method = "DELETE"
+	var name string
+	var path string
+	err := decoder.Decode(name, r.URL.Query())
+	if err != nil {
+		queryNotEnoughError(w, endpoint, method, "name")
+
+		return
+	}
+
+	err = decoder.Decode(path, r.URL.Query())
+	if err != nil {
+		queryNotEnoughError(w, endpoint, method, "path")
+
+		return
+	}
+
+	file, err := controller.DeleteFile(name, path)
+	if err != nil {
+		errorMessage := err.Error()
+		handlerRequestError(w, endpoint, method, http.StatusBadRequest, errorMessage)
+
+		return
+	}
+
+	domainWritebackToClient(file, w, endpoint, method)
 }
---
+++ server/error.go
@@ -0,0 +1,16 @@
+package server
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func queryNotEnoughError(
+	w http.ResponseWriter,
+	endpoint string,
+	method string,
+	query string,
+) {
+	errorMessage := fmt.Sprintf("Need query parameter: %s", query)
+	handlerRequestError(w, endpoint, method, http.StatusBadRequest, errorMessage)
+}
