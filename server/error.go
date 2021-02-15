6695d7cfede15efcf800693bf49e3deeb4fcd552
Kota Isozaki <49902177+isso0424@users.noreply.github.com>
2021-02-16 04:21:16 +0900

    feat: /files (#37)
    * feat: files
    
    * fix: lint

--- server/error.go
+++ server/error.go
@@ -14,3 +14,12 @@ func queryNotEnoughError(
 	errorMessage := fmt.Sprintf("Need query parameter: %s", query)
 	handlerRequestError(w, endpoint, method, http.StatusBadRequest, errorMessage)
 }
+
+func jsonParseError(
+	w http.ResponseWriter,
+	endpoint string,
+	method string,
+) {
+	errorMessage := "error occur in json pasing"
+	handlerRequestError(w, endpoint, method, http.StatusInternalServerError, errorMessage)
+}
