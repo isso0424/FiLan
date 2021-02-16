85257c8ca9fb3cb0ac79dcaea6715310a37c5408
isso0424 <isso04240129@gmail.com>
2021-02-16 09:36:05 +0900

    feat: files

--- server/getFiles.go
+++ server/getFiles.go
@@ -1,8 +1,47 @@
 package server
 
-import "net/http"
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
 
 func getFilesHandler(w http.ResponseWriter, r *http.Request) {
-	// nolint:godox
-	// TODO: write here
+	const endpoint = "/files"
+	const method = "GET"
+	var path string
+	err := decoder.Decode(path, r.URL.Query())
+	if err != nil {
+		queryNotEnoughError(w, endpoint, method, "path")
+
+		return
+	}
+
+	files, err := controller.GetFiles(path)
+	if err != nil {
+		errorMessage := "Internal server error"
+		handlerRequestError(w, endpoint, method, http.StatusInternalServerError, errorMessage)
+
+		return
+	}
+
+	filesModel := []fileModel{}
+
+	for _, file := range files {
+		fileModel := convertDomainToModel(file)
+		filesModel = append(filesModel, fileModel)
+	}
+
+	encoded, err := json.Marshal(filesModel)
+	if err != nil {
+		errorMessage := "error occur in json pasing"
+		handlerRequestError(w, endpoint, method, http.StatusInternalServerError, errorMessage)
+
+		return
+	}
+
+	_, err = w.Write(encoded)
+	if err != nil {
+		log.Println(err)
+	}
 }
